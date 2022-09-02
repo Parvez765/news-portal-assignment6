33e4bc30ce591c90aa6eb4d67b40adca0bda5cd7
Pervez <pervezhossain1998@gmail.com>
2022-09-02 21:33:23 +0600

    MenuBar Addedd

--- js/app.js
+++ js/app.js
@@ -1,10 +1,26 @@
 const loadCatagory = () => {
     fetch(`https://openapi.programming-hero.com/api/news/categories`)
         .then(res => res.json())
-        .then(data => console.log(data.data))
+        .then(data => displayMenuBar(data.data))
         .catch(error => console.log(error));
 }
 
+const displayMenuBar = (data) => {
+    const menuContainer = document.getElementById("menu-Container")
+    data.news_category.forEach(menu => {
+        const ulList = document.createElement('ul')
+        ulList.classList.add('nav')
+        ulList.innerHTML = `
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">${menu.category_name}</a>
+            </li>
+        `
+        menuContainer.appendChild(ulList)
+    })
 
+ 
+
+    
+}
 
 loadCatagory()

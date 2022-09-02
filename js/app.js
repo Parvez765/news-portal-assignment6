5d32665c17285b0b537afe94fb807e07c8e43596
Pervez <pervezhossain1998@gmail.com>
2022-09-02 20:49:05 +0600

    horizantal rular addedd

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const loadCatagory = () => {
+    fetch(`https://openapi.programming-hero.com/api/news/categories`)
+        .then(res => res.json())
+        .then(data => console.log(data.data))
+        .catch(error => console.log(error));
+}
+
+
+
+loadCatagory()
